e1664585395de8a259efe10afc20f66f9ebabd2f
Ernesto Novillo <ernesto.novillo@gmail.com>
2026-09-04 21:53:50 -0400

    More deployment woes.

--- reolink_video_library_manager/run.sh
+++ reolink_video_library_manager/run.sh
@@ -9,14 +9,14 @@ if [[ ! -f /opt/reolink_video_library_manager/video-event-card.js ]]; then
 	exit 1
 fi
 
-mkdir -p /homeassistant_config/www
-cp /opt/reolink_video_library_manager/video-event-card.js /homeassistant_config/www/video-event-card.js
+mkdir -p /config/www
+cp /opt/reolink_video_library_manager/video-event-card.js /config/www/video-event-card.js
 
-if [[ ! -s /homeassistant_config/www/video-event-card.js ]]; then
+if [[ ! -s /config/www/video-event-card.js ]]; then
 	bashio::log.error "Video Event Card was not installed in the Home Assistant configuration directory"
 	exit 1
 fi
 
-bashio::log.info "Installed Video Event Card at /homeassistant_config/www/video-event-card.js"
+bashio::log.info "Installed Video Event Card at /config/www/video-event-card.js"
 
 exec python3 /app.py
