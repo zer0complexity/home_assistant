c509cfeaa33b80cedc993586803fc92dddf05d45
Ernesto Novillo <ernesto.novillo@gmail.com>
2026-09-04 21:39:21 -0400

    Deploy the video-event-card to the right directory.

--- reolink_video_library_manager/run.sh
+++ reolink_video_library_manager/run.sh
@@ -2,8 +2,8 @@
 
 bashio::log.info "Starting Reolink Video Library Manager..."
 
-mkdir -p /config/www
-cp /opt/reolink_video_library_manager/video-event-card.js /config/www/video-event-card.js
-bashio::log.info "Installed Video Event Card at /config/www/video-event-card.js"
+mkdir -p /homeassistant_config/www
+cp /opt/reolink_video_library_manager/video-event-card.js /homeassistant_config/www/video-event-card.js
+bashio::log.info "Installed Video Event Card at /homeassistant_config/www/video-event-card.js"
 
 exec python3 /app.py
